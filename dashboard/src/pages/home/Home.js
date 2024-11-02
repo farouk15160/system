0bb9ce1bdcd91cfc8018e71b4342388aea8706e3
farouk15160 <farouk15160@gmail.com>
2024-11-02 15:13:15 +0100

    header rename , tabnle design , delete rows

--- dashboard/src/pages/home/Home.js
+++ dashboard/src/pages/home/Home.js
@@ -17,14 +17,13 @@ import {
   RadioGroup,
   HStack,
   Select,
+  Spinner,
 } from "@chakra-ui/react";
 import * as XLSX from "xlsx";
 import { DeleteIcon } from "@chakra-ui/icons";
 import Plot from "react-plotly.js";
 import { useAuth } from "../../context/AuthContext";
-
-import axios from "axios"; // Import axios
-
+import axios from "axios";
 const Home = () => {
   const toast = useToast();
   const { userData } = useAuth();
@@ -36,10 +35,12 @@ const Home = () => {
   const [plotType, setPlotType] = useState("single");
   const [graphType, setGraphType] = useState("line");
   const [plotedData, setPlotedData] = useState([]);
-
+  const [loading, setLoading] = useState(false);
+  const [headers, setHeaders] = useState([]);
   const handleFileUpload = (event) => {
     const uploadedFile = event.target.files[0];
     if (uploadedFile) {
+      setLoading(true);
       const reader = new FileReader();
       reader.onload = (e) => {
         const data = new Uint8Array(e.target.result);
@@ -47,36 +48,65 @@ const Home = () => {
         const firstSheetName = workbook.SheetNames[0];
         const worksheet = workbook.Sheets[firstSheetName];
         const jsonData = XLSX.utils.sheet_to_json(worksheet);
+
+        if (jsonData.length > 0) {
+          setHeaders(Object.keys(jsonData[0]));
+        }
+
         setFileData(jsonData);
         setFileName(uploadedFile.name);
         setFile(uploadedFile);
+        setLoading(false);
+
+        toast({
+          title: "File displayed successfully",
+          description: `${uploadedFile.name} has been uploaded`,
+          status: "success",
+          duration: 3000,
+          isClosable: true,
+        });
       };
       reader.readAsArrayBuffer(uploadedFile);
-
-      toast({
-        title: "File displayed successfully",
-        description: `${uploadedFile.name} has been uploaded`,
-        status: "success",
-        duration: 3000,
-        isClosable: true,
-      });
     }
   };
-
+  const handleHeaderChange = (index, newHeader) => {
+    const updatedHeaders = [...headers];
+    updatedHeaders[index] = newHeader;
+    setHeaders(updatedHeaders);
+    const updatedData = fileData.map((row) => {
+      const newRow = {};
+      Object.entries(row).forEach(([key, value], i) => {
+        newRow[updatedHeaders[i]] = value;
+      });
+      return newRow;
+    });
+    setFileData(updatedData);
+  };
   const handleCellChange = (rowIndex, key, value) => {
     const updatedData = [...fileData];
     updatedData[rowIndex][key] = value;
     setFileData(updatedData);
   };
-
   const handleRemoveFile = () => {
     setFile(null);
     setFileData([]);
     setFileName("");
     setYAxisKey("");
     setPlotedData([]);
+    setHeaders([]);
     toast({
       title: "File removed",
+      status: "info",
+      duration: 3000,
+      isClosable: true,
+    });
+  };
+
+  const handleDeleteRow = (rowIndex) => {
+    const updatedData = fileData.filter((_, index) => index !== rowIndex);
+    setFileData(updatedData);
+    toast({
+      title: "Row deleted",
       status: "info",
       duration: 3000,
       isClosable: true,
@@ -129,7 +159,6 @@ const Home = () => {
     }
 
     try {
-      // Send the fileData to your API
       const response = await axios.post(
         `${process.env.REACT_APP_API_URL}/upload/${
           userData.username.split("@")[0]
@@ -138,13 +167,13 @@ const Home = () => {
           y_axis_key: "temperature",
           plot_type: "single",
           graph_type: "line",
-          data: fileData, // Ensure fileData is formatted correctly for the server
+          data: fileData,
         }
       );
 
       toast({
         title: "Data saved successfully",
-        description: response.data.message, // Adjust according to your API response
+        description: response.data.message,
         status: "success",
         duration: 3000,
         isClosable: true,
@@ -170,6 +199,7 @@ const Home = () => {
             accept=".xlsx, .xls, .csv, .json"
             onChange={handleFileUpload}
           />
+          {loading && <Spinner size="xl" color="teal.500" />}{" "}
           {fileName && (
             <>
               <Box display="flex" justifyContent="space-between" width="100%">
@@ -180,45 +210,87 @@ const Home = () => {
                   colorScheme="red"
                 />
               </Box>
-              <Box overflowX="auto" width="100%">
-                <Table variant="striped" colorScheme="teal" size="sm">
-                  <Thead>
-                    <Th>Choose the Y-Axis</Th>
+              <Box
+                overflow="auto"
+                width="100%"
+                borderWidth={1}
+                borderRadius="md"
+                p={4}
+                boxShadow="md"
+                bg="white"
+              >
+                <Table variant="simple" size="sm">
+                  <Thead position="sticky" top={0} bg="gray.100" zIndex={1}>
                     <Tr>
-                      {fileData.length > 0 &&
-                        Object.keys(fileData[0]).map((key) => (
-                          <Th key={key}>
-                            <HStack>
-                              <Radio
-                                value={key}
-                                isChecked={yAxisKey === key}
-                                onChange={() => setYAxisKey(key)}
-                              />
-                              <Box>{key}</Box>
-                            </HStack>
-                          </Th>
-                        ))}
+                      {headers.map((header, index) => (
+                        <Th key={index} textAlign="center">
+                          <HStack spacing={2}>
+                            <Radio
+                              value={header}
+                              isChecked={yAxisKey === header}
+                              onChange={() => setYAxisKey(header)}
+                            />
+                            <Input
+                              value={header}
+                              onChange={(e) =>
+                                handleHeaderChange(index, e.target.value)
+                              }
+                              size="sm"
+                              variant="flushed"
+                            />
+                          </HStack>
+                        </Th>
+                      ))}
+                      <Th></Th> {/* For delete icon */}
                     </Tr>
                   </Thead>
                   <Tbody>
                     {fileData.map((row, rowIndex) => (
-                      <Tr key={rowIndex}>
+                      <Tr key={rowIndex} _hover={{ bg: "gray.50" }}>
                         {Object.entries(row).map(([key, value], cellIndex) => (
-                          <Td key={cellIndex}>
+                          <Td key={cellIndex} textAlign="center">
                             <Input
                               type="number"
                               value={value}
                               onChange={(e) =>
                                 handleCellChange(rowIndex, key, e.target.value)
                               }
+                              variant="flushed"
                             />
                           </Td>
                         ))}
+                        <Td>
+                          <IconButton
+                            icon={<DeleteIcon />}
+                            onClick={() => handleDeleteRow(rowIndex)}
+                            colorScheme="red"
+                            size="sm"
+                            aria-label="Delete row"
+                            _hover={{ color: "white", bg: "red.600" }}
+                          />
+                        </Td>
                       </Tr>
                     ))}
                   </Tbody>
                 </Table>
               </Box>
+              {/* <HStack spacing={4} mt={5}>
+                <Button colorScheme="teal" size="md" onClick={handleUpload}>
+                  Plot Data
+                </Button>
+                <Button colorScheme="blue" size="md" onClick={handleSave}>
+                  Save Data
+                </Button>
+                {plotedData.length > 0 && (
+                  <Button
+                    colorScheme="red"
+                    size="md"
+                    onClick={() => setPlotedData([])}
+                  >
+                    Reset Plots
+                  </Button>
+                )}
+              </HStack> */}
               <RadioGroup
                 onChange={setPlotType}
                 value={plotType}
@@ -287,7 +359,7 @@ const Home = () => {
                     overflow="hidden"
                   >
                     <Plot
-                      data={[data]} // Plot only one data set
+                      data={[data]}
                       layout={{
                         title: data.name,
                         xaxis: { title: "X-Axis" },
